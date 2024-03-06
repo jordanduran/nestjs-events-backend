ac53a581858fbfe1924d914df2624cfce6e9b56d
Jordan Duran <jordanduranm@gmail.com>
2024-03-06 14:09:54 -0500

    Entities primary keys and columns

--- src/event.entity.ts
+++ src/event.entity.ts
@@ -1,7 +1,19 @@
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity()
 export class Event {
+  @PrimaryColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   description: string;
+
+  @Column()
   when: Date;
+
+  @Column()
   address: string;
 }
