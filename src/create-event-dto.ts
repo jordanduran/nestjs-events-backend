1300b54ae4a8924eb574524df06a6a51e78f97b5
Jordan Duran <jordanduranm@gmail.com>
2024-03-12 12:11:24 -0400

    Input validation

--- src/create-event-dto.ts
+++ src/create-event-dto.ts
@@ -1,6 +1,12 @@
+import { IsDateString, Length } from 'class-validator';
+
 export class CreateEventDto {
+  @Length(5, 255, { message: 'The length of name is too short' })
   name: string;
+  @Length(5, 255)
   description: string;
+  @IsDateString()
   when: string;
+  @Length(5, 255)
   address: string;
 }
